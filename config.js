b2753d2cf1042321a4d6221134fdad9f86099c40
Asaju Enitan Michael <asajuenitan@gmail.com>
2022-04-26 13:32:42 +0100

    add express server, add endpoints

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-export default () => ({
-    port: parseInt(process.env.PORT) || 3000,
-    etherScanUrl: process.env.ETHERSCAN_URL,
-    etherScanApiToken: process.env.ETHERSCAN_API_TOKEN,
-});
-  
+module.exports = {
+  port: parseInt(process.env.PORT) || 3000,
+  etherScanUrl: process.env.ETHERSCAN_URL,
+  etherScanApiToken: process.env.ETHERSCAN_API_TOKEN,
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { port, etherScanUrl, etherScanApiToken } from "./config";
+const { port, etherScanUrl, etherScanApiToken } = require("./config");
+const axios = require("axios").default;
+const app = require("express")();
 
-
+require("dotenv").config();
 
 const getTransactions = (networksTransactions) => {
   // filter through the transactions
@@ -20,13 +22,25 @@ const getTransactions = (networksTransactions) => {
   return { btc, eth, usdt, trx };
 };
 
-
 const getEth = async (ethTrx) => {
-  const res = "";
-  console.log(res);
+  const values = [];
+  for (const trx of ethTrx) {
+    const { data } = await axios.get(
+      `${etherScanUrl}?module=account&action=txlistinternal&txhash=${trx.transaction_hash}&apikey=${etherScanApiToken}`
+    );
+    if (data.message === "OK") {
+      const result = data.result[0];
+      if (result) {
+        values.push({
+          hash: trx.transaction_hash,
+          isSameAsEthScan: result.value === trx.value,
+          hashValue: trx.value,
+          ethScanValue: result.value,
+        });
+      }
+    }
+  }
 };
-
-
 
 const getBtc = async (btcTrx) => {
   const res = "";
@@ -41,21 +55,37 @@ const getTrx = async (trxTransactions) => {
   console.log(res);
 };
 
-const { btc, eth, usdt, trx } = getTransactions([]); // destruct the results out to use for other functions
-
-// call functions here
-getEth(eth).then((trx) => {
-  console.log(trx);
+app.get("/", (req, res) => {
+  res.json({ message: "server up and running" });
 });
 
-getBtc(btc).then((trx) => {
-  console.log(trx);
+app.post("/transactions", async (req, res) => {
+  let transactions;
+  // serialize transactions according to networks
+  const { btc, eth, usdt, trx } = getTransactions(req.body.transactions);
+  transactions.btc = btc.length > 0 ? getBtc(btc) : [];
+  transactions.eth = eth.length > 0 ? getEth(eth) : [];
+  transactions.usdt = usdt.length > 0 ? getUsdt(usdt) : [];
+  transactions.trx = trx.length > 0 ? getTrx(trx) : [];
+
+  return res.json({
+    message: "fetched all transactions",
+    data: transactions,
+  });
 });
 
-getUsdt(usdt).then((trx) => {
-  console.log(trx);
+app.post("/transactions-by-network", async (req, res) => {
+  let transactions = [];
+  if (req.query.network === "eth") {
+    transactions = await getEth(req.body.transactions);
+  }
+  return res.json({
+    message: "fetched all transactions",
+    data: transactions,
+  });
 });
 
-getTrx(trx).then((trx) => {
-  console.log(trx);
+// start server
+app.listen(port, () => {
+  console.info("server is up and running: " + port);
 });
