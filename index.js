9b262873e40eb22b583a41ef479756dedda47445
Asaju Enitan Michael <asajuenitan@gmail.com>
2022-04-26 11:38:52 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,53 @@
+const getTransactions = (networksTransactions) => {
+  // filter through the transactions
+  const btc = networksTransactions.filter(
+    (transaction) => transaction.asset_symbol === "BTC"
+  );
+  const eth = networksTransactions.filter(
+    (transaction) => transaction.asset_symbol === "ETH"
+  );
+  const usdt = networksTransactions.filter(
+    (transaction) => transaction.asset_symbol === "USDT"
+  );
+  const trx = networksTransactions.filter(
+    (transaction) => transaction.asset_symbol === "TRX"
+  );
+
+  return { btc, eth, usdt, trx };
+};
+
+const getEth = async (ethTrx) => {
+  const res = "";
+  console.log(res);
+};
+const getBtc = async (btcTrx) => {
+  const res = "";
+  console.log(res);
+};
+const getUsdt = async (usdtTrx) => {
+  const res = "";
+  console.log(res);
+};
+const getTrx = async (trxTransactions) => {
+  const res = "";
+  console.log(res);
+};
+
+const { btc, eth, usdt, trx } = getTransactions([]); // destruct the results out to use for other functions
+
+// call functions here
+getEth(eth).then((trx) => {
+  console.log(trx);
+});
+
+getBtc(btc).then((trx) => {
+  console.log(trx);
+});
+
+getUsdt(usdt).then((trx) => {
+  console.log(trx);
+});
+
+getTrx(trx).then((trx) => {
+  console.log(trx);
+});
